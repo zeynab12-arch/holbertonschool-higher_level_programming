2d95d27d0327153d7a9e9e36967dc990fb457810
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-04-10 15:49:52 +0000

    12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz():
             print("FizzBuzz", end=" ")
         elif i % 5 == 0:
             print("Buzz", end=" ")
-        elif i % 3 ==0:
+        elif i % 3 == 0:
             print("Fizz", end=" ")
         else:
             print(i, end=" ")
