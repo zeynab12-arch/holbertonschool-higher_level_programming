c5e8fef3a4c70e72fe9c602efd55f25d0ce2e6af
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-04-10 18:44:02 +0000

    8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord('a') <= ord(str[i]) <= ord('z'):
+            print("{}".format(chr(ord(str[i]) - 32)), end="")
+        else:
+            print("{}".format(str[i]), end="")
