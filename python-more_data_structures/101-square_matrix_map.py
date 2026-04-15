a68fd5662f840a680b664b8eb107dcbdaaf06182
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-04-15 13:31:26 +0400

    Add square_matrix_map function to square matrix elements

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
-    if not my_list:
-        return 0
-    nom = 0
-    denom = 0
-
-    for i in my_list:
-        nom += i[0]*i[1]
-        denom += i[1]
-
-    return nom/denom
+def square_matrix_map(matrix=[]):
+    return list(map(lambda x: list(map(lambda y: y**2, x)), matrix))
