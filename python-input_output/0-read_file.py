a026bb44c1169d185e3f08ce3a713436cb18d373
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-04-22 07:34:19 +0000

    )

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Module that reads a file and prints it to stdout."""
 
+
 def read_file(filename=""):
+    """Read a UTF-8 file and print its content to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
