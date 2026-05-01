f43d46aed66a3ae1a564ca93ebd1dacf76a34581
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-05-01 10:18:20 +0400

    Create 0-add_integer.py

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module defines a function that adds two integers."""
+
+
+def add_integer(a, b=98):
+    """Add two numbers"""
+    if not (isinstance(a, int) or isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if not (isinstance(b, int) or isinstance(b, float)):
+        raise TypeError("b must be an integer")
+
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+
+    return a + b
