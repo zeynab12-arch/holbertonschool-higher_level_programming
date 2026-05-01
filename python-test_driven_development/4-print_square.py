7c3ac4f92a0ad4728188367330935b9719c1b5fc
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-05-01 10:19:48 +0400

    Create 4-print_square.py

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module prints a square using the # character."""
+
+
+def print_square(size):
+    """Print a square with the character #."""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
