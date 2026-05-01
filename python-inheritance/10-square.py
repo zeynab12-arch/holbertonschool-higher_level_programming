e1d9a55df9eb1b6bf38c5d6568979ea7ee9dc459
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-05-01 09:37:55 +0400

    Create 10-square.py

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''This module is child class of Rectangle.'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''Is child of BaseGeometry.'''
+
+    def __init__(self, size):
+        super().integer_validator("size", size)
+
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
