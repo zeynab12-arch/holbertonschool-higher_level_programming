462d6e652d82d1cac01b020ba78b2c03dc9b0619
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-05-10 21:35:29 +0000

    )

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,9 +1,5 @@
-#!/usr/bin/python3
-'''This module returns json->obj.'''
 import json
 
 
 def from_json_string(my_str):
-    '''Returns json->obj.'''
     return json.loads(my_str)
-    
--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,2 @@
-#!/usr/bin/python3
-'''This module  returns the dictionary description.'''
-
-
 def class_to_json(obj):
-    '''returns the dictionary description.'''
     return obj.__dict__
-	
