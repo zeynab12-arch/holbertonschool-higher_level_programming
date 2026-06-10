6169ca6a1657fee9aaaa60ab83c5d95d98c6abaa
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-06-10 09:18:57 +0000

    )

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
 
-function add (a, b) {
-  return a + b;
-}
-
-const a = Number(process.argv[2]);
-const b = Number(process.argv[3]);
-
-console.log(add(a, b));
+const myObject = {
+  type: 'object',
+  value: 12
+  };
+  
+  console.log(myObject);
+  
+  myObject.value = 89;
+  
+  console.log(myObject);
+  
