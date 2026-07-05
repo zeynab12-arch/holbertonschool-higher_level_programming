c96d5ac8b1c3f36ce86020383d799576fd492810
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-07-05 08:12:57 +0000

    )

--- javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const line of array) {
-  console.log(line);
-}
--- javascript-warm_up/7-multi_c.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const x = parseInt(process.argv[2], 10);
-if (Number.isInteger(x)) {
-  for (let i = 0; i < x; i++) {
-    console.log('C is fun');
-  }
-} else {
-  console.log('Missing number of occurrences');
-}
