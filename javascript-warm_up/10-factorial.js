3250b07749d02517ad1e64b44faa0c46b42d8670
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-07-11 20:36:03 +0000

    )

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 function factorial(n) {
-  if (n === 0 || isNaN(n)) {
+  if (isNaN(n) || n <= 1) {
     return 1;
   }
-  return n * factorial(n - 1);
+return n * factorial(n - 1);
 }
 console.log(factorial(Number(process.argv[2])));
