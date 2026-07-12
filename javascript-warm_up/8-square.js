3efda9c0f809d7bb71365c31011e80176e2057be
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-07-12 13:55:24 +0000

    )

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const num = parseInt(process.argv[2]);
+const num = parseInt(process.argv[2], 10);
 if (Number.isInteger(num)) {
-    for (let i = 0; i < num; i++){
-        console.log('X'.repeat(num));
-    }
+  for (let i = 0; i < num; i++) {
+    console.log('X'.repeat(num));
+  }
 } else {
-    console.log('Missing size');
+  console.log('Missing size');
 }
