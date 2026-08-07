fb49016cd23a188c7edbb48c6f38b387e73c15b0
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-08-07 10:25:50 +0000

    )

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
+const args = process.argv.slice(2).map((value) => parseInt(value, 10));
 
-function secondBiggest (array) {
-    if (array.length === 0 || array.length === 1) {
-      return 0;
-    } else {
-      const sortedArray = array.sort((a, b) => a - b);
-      return sortedArray[sortedArray.length - 2];
-    }
-  }
-  
-  const argsArray = process.argv.slice(2).map(Number);
-  console.log(secondBiggest(argsArray));
+if (args.length < 2) {
+  console.log(0);
+} else {
+  const uniqueValues = [...new Set(args)].sort((left, right) => right - left);
+  console.log(uniqueValues.length > 1 ? uniqueValues[1] : 0);
+}
--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-const add = require('./13-add').add;
-console.log(add(3, 5));
+function add (a, b) {
+    return a + b;
+  }
+  
+  module.exports = { add };
